0a50c3274f7ea1cf394c88ab082777a13b794735
Valentin <vcalomino@fi.uba.ar>
2025-10-23 16:30:29 -0300

    show car sprites on screen

--- client/main.cpp
+++ client/main.cpp
@@ -1,13 +1,20 @@
 #include <exception>
 #include <iostream>
+#include <map>
+#include <string>
 
 #include <SDL2/SDL.h>
 #include <SDL2pp/SDL2pp.hh>
+#include <SDL2pp/SDLImage.hh>
+#include <SDL2pp/Texture.hh>
 
 #include "common/foo.h"
 
+using SDL2pp::Rect;
 using SDL2pp::Renderer;
 using SDL2pp::SDL;
+using SDL2pp::SDLImage;
+using SDL2pp::Texture;
 using SDL2pp::Window;
 
 
@@ -15,21 +22,30 @@ int main() try {
     // Initialize SDL library
     SDL sdl(SDL_INIT_VIDEO);
 
+
     // Create main window: 640x480 dimensions, resizable, "SDL2pp demo" title
-    Window window("SDL2pp demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480,
+    Window window("Need for speed", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480,
                   SDL_WINDOW_RESIZABLE);
 
     // Create accelerated video renderer with default driver
     Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    // Clear screen
+    Texture texture(renderer, "../client/assets/need-for-speed/cars/cars.png");
+
+    std::map<std::string, Rect> cars = {{"red", Rect(0, 64, 64, 64)},
+                                        {"blue", Rect(64, 128, 64, 64)},
+                                        {"green", Rect(128, 0, 64, 64)}};
+
+    SDL2pp::Rect srcRect(3 * 64, 2 * 64, 64, 64);
+    SDL2pp::Rect destRect(100, 100, 64, 64);
+
     renderer.Clear();
-
-    // Show rendered frame
+    renderer.Copy(texture, cars["red"], Rect(100, 100, 64, 64));
+    renderer.Copy(texture, cars["blue"], Rect(200, 100, 64, 64));
+    renderer.Copy(texture, cars["green"], Rect(300, 100, 64, 64));
     renderer.Present();
-
-    // 5 second delay
     SDL_Delay(5000);
+
 
     // Here all resources are automatically released and library deinitialized
     return 0;
