22b1ab5855381bfc88a798ec798d5d54efe4f640
Valentin <vcalomino@fi.uba.ar>
2025-11-05 17:21:37 -0300

    change player id from string to int

--- client/world/player.h
+++ client/world/player.h
@@ -7,18 +7,18 @@
 
 class Player {
 public:
-    Player(const std::string& id, const std::string& carType, float x, float y);
+    Player(const int id, const std::string& carType, float x, float y);
 
     void UpdateFromNetwork(float x, float y, float angle);
 
     float GetX() const { return x_; }
     float GetY() const { return y_; }
     float GetAngle() const { return angle_; }
-    const std::string& GetId() const { return id_; }
+    int GetId() const { return id_; }
     std::string GetSpriteForAngle(float angle) const;
 
 private:
-    std::string id_;
+    int id_;
     std::string carType_;
     std::string currentSprite_;
     float x_;
