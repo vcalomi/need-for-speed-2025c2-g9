82578cefdecf4b7d3921e5bac21c47095e48ceb9
Valentin <vcalomino@fi.uba.ar>
2025-11-05 18:29:09 -0300

    add case to process new player dto

--- client/core/game.cpp
+++ client/core/game.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 
 #include "../../common/Dto/dto.h"
+#include "../../common/Dto/player.h"
 #include "../../common/Dto/vehicle.h"
 
 Game::Game(Client& client):
@@ -13,29 +14,33 @@ Game::Game(Client& client):
         world_(),
         inputSystem_(),
         rendererSystem_(engine_.GetRenderer(), resources_.GetCarSprites()),
-        map_(engine_.GetRenderer(), "../client/assets/need-for-speed/cities/san_andreas.png"),
-        mockServer_() {
-
-    const auto& players = mockServer_.GetPlayers();
-    for (const auto& [id, p]: players) {
-        world_.AddPlayer(id, "CamionetaRoja", p.x, p.y, id == "1");
-    }
-
+        map_(engine_.GetRenderer(), "../client/assets/need-for-speed/cities/san_andreas.png") {
     audioManager_.PlayBackgroundMusic("../client/assets/need-for-speed/music/background.wav");
 }
 
 void Game::processDto(const std::shared_ptr<Dto>& dto) {
     ActionCode code = static_cast<ActionCode>(dto->return_code());
     switch (code) {
+        case ActionCode::SEND_PLAYER: {
+            auto playerDto = std::dynamic_pointer_cast<PlayerDto>(dto);
+            if (playerDto) {
+                std::cout << "Procesando jugador id=" << (int)playerDto->id << std::endl;
+                if (!world_.HasPlayer(playerDto->id)) {
+                    world_.AddPlayer(playerDto->id, playerDto->carType, false);
+                    std::cout << "Jugador agregado al mundo, id=" << (int)playerDto->id
+                              << std::endl;
+                }
+            }
+            break;
+        }
         case ActionCode::SEND_CARS: {
             auto vehicleDto = std::dynamic_pointer_cast<VehicleDto>(dto);
             if (vehicleDto) {
-                std::cout << "Procesando vehículo id=" << (int)vehicleDto->id << " pos("
-                          << vehicleDto->x << ", " << vehicleDto->y
-                          << ") rot=" << vehicleDto->rotation << std::endl;
-                // acá podrías actualizar el estado del juego (ej: mundo.updateCar(vehicleDto))
-                world_.UpdateFromServer(std::to_string(vehicleDto->id), vehicleDto->x,
-                                        vehicleDto->y, vehicleDto->rotation);
+                // std::cout << "Procesando vehículo id=" << (int)vehicleDto->id << " pos("
+                //           << vehicleDto->x << ", " << vehicleDto->y
+                //           << ") rot=" << vehicleDto->rotation << std::endl;
+                world_.UpdateFromServer(vehicleDto->id, vehicleDto->x, vehicleDto->y,
+                                        vehicleDto->rotation);
             }
             break;
         }
@@ -48,23 +53,15 @@ void Game::processDto(const std::shared_ptr<Dto>& dto) {
 
 void Game::Run() {
     bool running = true;
-    // Uint64 prevTicks = SDL_GetTicks64();
-
     while (running) {
-        // Uint64 currentTicks = SDL_GetTicks64();
-        // float delta = (currentTicks - prevTicks) / 1000.f;
-        // prevTicks = currentTicks;
 
         // --- INPUT ---
         inputSystem_.PollEvents(running);
-        Dto input = inputSystem_.GetInputByte();
+        Dto input = inputSystem_.GetInputByte(this->world_.GetLocalPlayer().GetId());
         if (input.return_code() != 0) {
             client_.getSenderQueue().try_push(std::make_shared<Dto>(input));
             std::cout << "Input enviado: " << +input.return_code() << std::endl;
         }
-
-        // --- SIMULACIÓN SERVIDOR ---
-        // mockServer_.Update(delta);
 
         // --- MENSAJES DEL SERVIDOR ---
         std::shared_ptr<Dto> dto = nullptr;
