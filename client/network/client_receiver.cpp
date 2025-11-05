53ed9b685f49a406bfe1329d26b58c5096676a2c
Valentin <vcalomino@fi.uba.ar>
2025-11-05 20:10:22 -0300

    push dto directly to queue in receiver

--- client/network/client_receiver.cpp
+++ client/network/client_receiver.cpp
@@ -15,28 +15,7 @@ void ClientReceiver::run() {
         while (should_keep_running() && !protocol.isClientConnected()) {
             std::cout << "ClientReceiver: Waiting to receive DTO..." << std::endl;
             std::shared_ptr<Dto> dto = protocol.receiveDTO();
-            ActionCode code = static_cast<ActionCode>(dto->return_code());
-
-            switch (code) {
-                case ActionCode::SEND_CARS: {
-                    auto vehicleDto = std::dynamic_pointer_cast<VehicleDto>(dto);
-                    if (vehicleDto) {
-                        std::cout << "Vehicle id=" << (int)vehicleDto->id << " x=" << vehicleDto->x
-                                  << " y=" << vehicleDto->y << " rot=" << vehicleDto->rotation
-                                  << std::endl;
-                        queue.push(vehicleDto);
-                    } else {
-                        std::cout << "Error: Expected VehicleDto but got different type"
-                                  << std::endl;
-                    }
-                    break;
-                }
-                default: {
-                    std::cout << "Received DTO with code: " << (int)code << std::endl;
-                    queue.push(dto);
-                    break;
-                }
-            }
+            queue.push(dto);
         }
     } catch (const SocketClosed& e) {
         return;
