5b46621741abb8b88be0ff7605f8fe2c007518e8
Valentin Calomino <vcalomino@fi.uba.ar>
2025-11-10 16:27:16 -0300

    draw username above player

--- client/graphics/renderer_system.cpp
+++ client/graphics/renderer_system.cpp
@@ -1,14 +1,34 @@
 #include "./renderer_system.h"
 
+#include <iostream>
+
+#include <SDL2/SDL_ttf.h>
+
 #define HALF_DIVISOR 2.0f
 
 RendererSystem::RendererSystem(SDL2pp::Renderer& renderer, SpriteSheet& cars):
-        renderer_(renderer), cars_(cars) {}
+        renderer_(renderer), cars_(cars) {
+    font_ = TTF_OpenFont("../client/lobby/assets/Tektur-SemiBold.ttf", 14);
+    if (!font_) {
+        std::cerr << "[RendererSystem] Failed to load font: " << TTF_GetError() << std::endl;
+    } else {
+        std::cout << "[RendererSystem] Font loaded successfully.\n";
+    }
+}
+
+RendererSystem::~RendererSystem() {
+    if (font_) {
+        TTF_CloseFont(font_);
+        font_ = nullptr;
+    }
+}
 
 void RendererSystem::Render(const World& world, Map& map, const Camera& camera, Minimap& minimap) {
     renderer_.Clear();
     map.Render(renderer_, camera);
-    for (const auto& [id, player]: world.GetPlayers()) DrawPlayer(player, camera);
+    for (const auto& [id, player]: world.GetPlayers()) {
+        DrawPlayer(player, camera);
+    }
     minimap.Render(world, camera);
     renderer_.Present();
 }
@@ -27,4 +47,30 @@ void RendererSystem::DrawPlayer(const Player& player, const Camera& camera) {
 
     Rect dest(drawX, drawY, src.GetW(), src.GetH());
     renderer_.Copy(cars_.GetTexture(), src, dest);
+    DrawTextAbove(font_, player.GetUsername(), drawX, drawY, src);
+}
+
+void RendererSystem::DrawTextAbove(TTF_Font* font, const std::string& text, float drawX,
+                                   float drawY, const Rect& src) {
+    if (!font)
+        return;
+
+    SDL_Color color = {255, 255, 255, 255};
+    SDL_Surface* textSurface = TTF_RenderText_Blended(font, text.c_str(), color);
+    if (!textSurface)
+        return;
+
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer_.Get(), textSurface);
+    if (textTexture) {
+        int textW, textH;
+        SDL_QueryTexture(textTexture, nullptr, nullptr, &textW, &textH);
+
+        SDL_Rect textRect = {static_cast<int>(drawX + src.GetW() / 2 - textW / 2),
+                             static_cast<int>(drawY - textH - 5), textW, textH};
+
+        SDL_RenderCopy(renderer_.Get(), textTexture, nullptr, &textRect);
+        SDL_DestroyTexture(textTexture);
+    }
+
+    SDL_FreeSurface(textSurface);
 }
--- client/graphics/renderer_system.h
+++ client/graphics/renderer_system.h
@@ -1,19 +1,28 @@
 #pragma once
+#include <string>
+
+#include <SDL2/SDL_ttf.h>
 #include <SDL2pp/SDL2pp.hh>
 
+#include "../core/camera.h"
 #include "../core/map.h"
+#include "../graphics/spritesheet.h"
 #include "../ui/minimap.h"
 #include "../world/world.h"
-#include "./spritesheet.h"
 
 class RendererSystem {
 public:
     RendererSystem(SDL2pp::Renderer& renderer, SpriteSheet& cars);
+    ~RendererSystem();
+
     void Render(const World& world, Map& map, const Camera& camera, Minimap& minimap);
+    void DrawPlayer(const Player& player, const Camera& camera);
 
 private:
     SDL2pp::Renderer& renderer_;
     SpriteSheet& cars_;
+    TTF_Font* font_ = nullptr;
 
-    void DrawPlayer(const Player& player, const Camera& camera);
+    void DrawTextAbove(TTF_Font* font, const std::string& text, float drawX, float drawY,
+                       const Rect& src);
 };
