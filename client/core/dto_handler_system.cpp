eddc0de7ef8c2a8cb4844e034b13e9f87f9bb63c
Valentin Calomino <vcalomino@fi.uba.ar>
2025-11-10 18:12:16 -0300

    encapsulate dto processing in a class

---
+++ client/core/dto_handler_system.cpp
@@ -0,0 +1,44 @@
+#include "./dto_handler_system.h"
+
+DtoHandlerSystem::DtoHandlerSystem(World& world, Client& client): world_(world), client_(client) {}
+
+void DtoHandlerSystem::Process(const std::shared_ptr<Dto>& dto) {
+    if (!dto)
+        return;
+
+    ActionCode code = static_cast<ActionCode>(dto->return_code());
+    switch (code) {
+        case ActionCode::SEND_PLAYER: {
+            auto playerDto = std::dynamic_pointer_cast<PlayerDto>(dto);
+            if (!playerDto)
+                return;
+
+            const auto& username = playerDto->username;
+            std::cout << "[DtoHandler] Received player: " << username << std::endl;
+
+            if (!world_.HasPlayer(username)) {
+                world_.AddPlayer(username, playerDto->Type, username == client_.getUsername());
+                std::cout << "[DtoHandler] Player added: " << username << std::endl;
+            }
+            break;
+        }
+
+        case ActionCode::SEND_CARS: {
+            auto vehicleDto = std::dynamic_pointer_cast<VehicleDto>(dto);
+            if (!vehicleDto)
+                return;
+
+            std::cout << "[DtoHandler] Vehicle update: " << vehicleDto->username << " pos("
+                      << vehicleDto->x << ", " << vehicleDto->y << ") rot=" << vehicleDto->rotation
+                      << std::endl;
+
+            world_.UpdateFromServer(vehicleDto->username, vehicleDto->x, vehicleDto->y,
+                                    vehicleDto->rotation);
+            break;
+        }
+
+        default:
+            std::cout << "[DtoHandler] Unhandled DTO code=" << (int)code << std::endl;
+            break;
+    }
+}
---
+++ client/core/dto_handler_system.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <memory>
+
+#include "../../common/Dto/dto.h"
+#include "../../common/Dto/player.h"
+#include "../../common/Dto/player_move.h"
+#include "../../common/Dto/vehicle.h"
+#include "../network/client.h"
+#include "../world/world.h"
+
+class DtoHandlerSystem {
+public:
+    DtoHandlerSystem(World& world, Client& client);
+    void Process(const std::shared_ptr<Dto>& dto);
+
+private:
+    World& world_;
+    Client& client_;
+};
