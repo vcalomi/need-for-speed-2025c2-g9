d18a250dffce8579ebfffec131c82d9a3bba1e16
Valentin <vcalomino@fi.uba.ar>
2025-11-30 15:48:26 -0300

    add debug print

--- client/systems/dto_handlers/send_cars_handler.cpp
+++ client/systems/dto_handlers/send_cars_handler.cpp
@@ -6,7 +6,8 @@ std::shared_ptr<Event> SendCarsHandler::Handle(const std::shared_ptr<Dto>& dto) 
     auto d = std::dynamic_pointer_cast<VehicleDto>(dto);
     if (!d)
         return nullptr;
-    std::cout << d->speed << std::endl;
+
+    std::cout << "[SendCarsHandler] Is above bridge: " << d->isAboveBridge << std::endl;
     return std::make_shared<PlayerStateUpdatedEvent>(d->username, d->x, d->y, d->rotation, d->speed,
                                                      d->isAboveBridge);
 }
